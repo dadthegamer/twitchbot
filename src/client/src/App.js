e6de2cdbec5a2ed3a0fda9fdbd9baf79a2fe6909
Craig Bailey <bailey_craig@me.com>
2023-09-18 03:55:36 -0500

    updates to client routing

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,22 +1,45 @@
 // App.js
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Outlet } from 'react-router-dom';
 import Users from './components/Users';
 import SideNavbar from './components/SideNavBar';
 import TopNavbar from './components/TopNavBar';
 import Currency from './components/Currency';
+import Goal from './components/overlayComponents/goalBar';
 
 function App() {
   return (
-    // Wrap the Routes component in a div with the class name "container"
-    <div className="main-container">
-      <SideNavbar />
+    <Routes>
+      {/* Main Routes */}
+      <Route path="/" element={<MainLayout />}>
+        <Route path="users" element={<Users />} />
+        <Route path="currency" element={<Currency />} />
+      </Route>
+
+      {/* Overlay Routes */}
+      <Route path="/overlay" element={<OverlayLayout />}>
+        <Route path="progressbar" element={<Goal />} />
+      </Route>
+    </Routes>
+  );
+}
+
+
+function MainLayout() {
+  return (
+    <div className="main-layout">
       <TopNavbar />
-      <Routes>
-        <Route path="/users" element={<Users />} />
-        <Route path="/currency" element={<Currency />} />
-      </Routes>
+      <SideNavbar />
+
+      <Outlet />
+
     </div>
+  );
+}
+
+function OverlayLayout() {
+  return (
+    <Outlet />
   );
 }
 
---
+++ src/client/src/components/overlayComponents/goalBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../styles/overlay/goalBar.css';
+
+
+function Goal() {
+    return (
+        <div className="goal-main-container">
+            <h1>Goal</h1>
+        </div>
+    );
+}
+
+export default Goal;
