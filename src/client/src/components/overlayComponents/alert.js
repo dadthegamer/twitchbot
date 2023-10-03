6da405260ff0939f471d3af9780d2debb3485ab6
Craig Bailey <bailey_craig@me.com>
2023-10-03 02:33:36 -0500

    setup alert css

--- src/client/src/components/overlayComponents/alert.js
+++ src/client/src/components/overlayComponents/alert.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGift, faStar, faBolt, faHeart, faGem, faDollarSign } from '@fortawesome/free-solid-svg-icons';
+import '../../styles/alert.css';
 
 
 function Alert() {
