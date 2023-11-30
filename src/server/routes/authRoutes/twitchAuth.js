a60644997cc161b517fd942dc8e7321cde25b0c1
Craig Bailey <bailey_craig@me.com>
2023-11-30 11:00:22 -0600

    fixed uri

--- src/server/routes/authRoutes/twitchAuth.js
+++ src/server/routes/authRoutes/twitchAuth.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { twitchClientId, twitchClientSecret, redirectUri } from '../../config/environmentVars.js';
+import { twitchClientId, uri } from '../../config/environmentVars.js';
 import logger from '../../utilities/logger.js';
 
 const router = Router();
@@ -7,7 +7,7 @@ const router = Router();
 router.get('/', (req, res) => {
     logger.info('GET /auth/twitch');
     const clientId = twitchClientId;
-    const redirectUriEncoded = encodeURIComponent(redirectUri);
+    const redirectUriEncoded = encodeURIComponent(uri);
     const scopes = encodeURIComponent(
         'user:read:email'
     );
