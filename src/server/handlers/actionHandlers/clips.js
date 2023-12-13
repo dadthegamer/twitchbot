5655492a06cc2081f2197c92fb105f48d7f10a04
Craig Bailey <bailey_craig@me.com>
2023-12-13 13:05:44 -0600

    updates

--- src/server/handlers/actionHandlers/clips.js
+++ src/server/handlers/actionHandlers/clips.js
@@ -10,13 +10,29 @@ export async function createClip() {
             chatClient.say('Stream is not live, cannot create clip.');
         } else {
             const clip = await twitchApi.createTwitchClip();
-            setTimeout(async () => {
-                const clipData = await twitchApi.getTwitchClipById(clip);
-                const clipUrl = clipData.url;
-                if (clipUrl) {
-                    chatClient.say(`Clip created: ${clipUrl}`);
-                }
-            }, 5000);
+            const clipData = await twitchApi.getTwitchClipById(clip);
+            let tries = 0;
+            let maxTries = 15;
+            // Do a while loop to check if the clip data is not null
+            while (!clipData && tries < maxTries) {
+                tries++;
+                await new Promise(resolve => setTimeout(resolve, 1000));
+                // Refresh clipData on each iteration
+                clipData = await twitchApi.getTwitchClipById(clip);
+            }
+
+            if (clipData && clipData.url) {
+                chatClient.say(`Clip created: ${clipData.url}`);
+            } else {
+                logger.error(`Error in createClip handler: Clip data is null`);
+            }
+            // setTimeout(async () => {
+            //     const clipData = await twitchApi.getTwitchClipById(clip);
+            //     const clipUrl = clipData.url;
+            //     if (clipUrl) {
+            //         chatClient.say(`Clip created: ${clipUrl}`);
+            //     }
+            // }, 5000);
         }
     }
     catch (err) {
