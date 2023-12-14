90192afd485fa86f4c8148c7b99e0966b02de27a
Craig Bailey <bailey_craig@me.com>
2023-12-14 17:30:55 -0600

    updates

--- src/server/services/openAi.js
+++ src/server/services/openAi.js
@@ -6,39 +6,54 @@ const openai = new OpenAI({
 });
 
 export async function openAiRequest(message) {
-    const chatCompletion = await openai.chat.completions.create({
-        messages: [{ role: 'user', content: message }],
-        model: 'gpt-3.5-turbo',
-    });
-    const response = chatCompletion.choices[0].message.content;
-    return response;
+    try {
+        const chatCompletion = await openai.chat.completions.create({
+            messages: [{ role: 'user', content: message }],
+            model: 'gpt-3.5-turbo',
+        });
+        const response = chatCompletion.choices[0].message.content;
+        return response;
+    }
+    catch (error) {
+        logger.error(error);
+    }
 }
 
 // Function to make sure the content is appropriate for my twitch chat
 export async function openAiRequestIsAppropriate(message) {
-    const chatCompletion = await openai.chat.completions.create({
-        messages: [{
-            "role": "system",
-            "content": "You will be provided with a message to analyze to determine rather it is appropriate for my twitch chat. Respond in json with a key of response with either true or false. If true, then the text is appropriate for my twitch chat. If false, then the text is not. Curse words are allowed, however no bigotry, racism, or hate speech is allowed."
-        },
-        {
-            "role": "user",
-            "content": message
-        }],
-        model: 'gpt-3.5-turbo',
-    });
-    const response = chatCompletion.choices[0].message.content;
-    // Parse the response into a json object
-    const responseJson = JSON.parse(response);
-    return responseJson.response;
+    try {
+        const chatCompletion = await openai.chat.completions.create({
+            messages: [{
+                "role": "system",
+                "content": "You will be provided with a message to analyze to determine rather it is appropriate for my twitch chat. Respond in json with a key of response with either true or false. If true, then the text is appropriate for my twitch chat. If false, then the text is not. Curse words are allowed, however no bigotry, racism, or hate speech is allowed."
+            },
+            {
+                "role": "user",
+                "content": message
+            }],
+            model: 'gpt-3.5-turbo',
+        });
+        const response = chatCompletion.choices[0].message.content;
+        // Parse the response into a json object
+        const responseJson = JSON.parse(response);
+        return responseJson.response;
+    }
+    catch (error) {
+        logger.error(error);
+    }
 }
 
 // Function to roast the user
 export async function roastUser() {
-    const chatCompletion = await openai.chat.completions.create({
-        messages: [{ role: 'user', content: 'Roast the u' }],
-        model: 'gpt-3.5-turbo',
-    });
-    const response = chatCompletion.choices[0].message.content;
-    return response;
+    try {
+        const chatCompletion = await openai.chat.completions.create({
+            messages: [{ role: 'user', content: 'Roast the u' }],
+            model: 'gpt-3.5-turbo',
+        });
+        const response = chatCompletion.choices[0].message.content;
+        return response;
+    }
+    catch (error) {
+        logger.error(error);
+    }
 }
