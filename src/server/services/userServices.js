fe16cad7fc4f3b1f532ec2eab344bc842f827132
Craig Bailey <bailey_craig@me.com>
2024-01-25 16:51:07 -0600

    updates

--- src/server/services/userServices.js
+++ src/server/services/userServices.js
@@ -1242,8 +1242,6 @@ class UsersDB {
             );
             const users = await this.dbConnection.collection(this.collectionName).find({}).toArray();
             this.cache.set('users', users);
-            logger.info(`Increased view time for ${userIds} by ${minutes} minutes`);
-            console.log(`Increased view time for ${userIds} by ${minutes} minutes`);
             return result;
         } catch (error) {
             logger.error(`Error in increaseViewTimeForUsers: ${error}`);
