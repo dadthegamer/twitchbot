e65212a419e5d218738ee908e80f9ae7cbf994fe
Craig Bailey <bailey_craig@me.com>
2024-02-28 20:02:35 -0600

    updates

--- src/server/handlers/actionHandlers/lateHandler.js
+++ src/server/handlers/actionHandlers/lateHandler.js
@@ -2,12 +2,11 @@ import logger from '../../utilities/logger.js';
 import { cache, chatClient, gameService } from '../../config/initializers.js';
 
 
-let lateUsers = cache.get('lateUsers') || [];
-
 export async function lateHandler(userId, displayName) {
     try {
         const late = cache.get('late');
         if (late) {
+            const lateUsers = cache.get('lateUsers');
             // See if user is already in the late list
             const user = lateUsers.find(u => u.userId === userId);
             if (user) {
